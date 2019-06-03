2f45d346448ef867435162b4a9ecef9dfb932cb2
Karen Khalifah <karen.khalifah@gmail.com>
2019-06-02 22:54:49 -0400

    added wowjs functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 $(document).ready(() => {
+    // wowjs.uk/docs.html for styling options
+     new WOW().init();
+    
  $('.js-wp-2').waypoint(direction => {
  $('.js-wp-2').addClass('animated slideInUp');
  }, {
  offset: '30%'
  });
+    
+ $('.js-wp-3').waypoint(direction => {
+ $('.js-wp-3').addClass('animated fadeIn');
+ }, {
+ offset: '70%'
+ });
+    
+    $('.phone-btn').delay(1000).animate({bottom: '+=-3'}, 200);
+    $('.phone-btn').delay(300).animate({top: '+=-3'}, 300);
+    
+    
+  
+    
+    
+    
 });
